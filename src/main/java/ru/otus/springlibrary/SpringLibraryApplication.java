ce1c6a719b7c66957bc262749b6e5c4c202586d3
Grigorii Liullin <5632433+GrigoriyLyullin@users.noreply.github.com>
2019-03-17 20:55:01 +0300

    Initial commit

---
+++ src/main/java/ru/otus/springlibrary/SpringLibraryApplication.java
@@ -0,0 +1,13 @@
+package ru.otus.springlibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/otus/springlibrary/SpringLibraryApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.otus.springlibrary;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringLibraryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
