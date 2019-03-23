b4095d56f11fa196cf29044d7e2d17e49f58830c
Grigorii Liullin <grigorii.liullin@t-systems.com>
2019-03-23 19:04:28 +0300

    Add travis file

--- src/main/java/ru/otus/springlibrary/SpringLibraryApplication.java
+++ src/main/java/ru/otus/springlibrary/SpringLibraryApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringLibraryApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringLibraryApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLibraryApplication.class, args);
+    }
 
 }
