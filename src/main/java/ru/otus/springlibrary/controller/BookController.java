f594553cb1de943eca9388e2145ce938b4bbfd73
Grigorii Liullin <5632433+GrigoriyLyullin@users.noreply.github.com>
2019-05-01 22:03:06 +0300

    Implement remove book functionality

--- src/main/java/ru/otus/springlibrary/controller/BookController.java
+++ src/main/java/ru/otus/springlibrary/controller/BookController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.bson.types.ObjectId;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -49,4 +50,17 @@ public class BookController {
         return "redirect:/books";
     }
 
+    @GetMapping("/books/remove")
+    public String showRemoveBookForm(@RequestParam ObjectId id,
+                                     Model model) {
+        Book book = bookService.findById(id);
+        model.addAttribute("book", book);
+        return "remove_book";
+    }
+
+    @DeleteMapping("/books/remove")
+    public String removeBook(@RequestParam ObjectId id) {
+        bookService.delete(id);
+        return "redirect:/books";
+    }
 }
